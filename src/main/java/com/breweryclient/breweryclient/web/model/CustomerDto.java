0905007bc98976167d97a8ec2e5a90f3e500ca9d
DonneGri <grigalionyte.donata@gmail.com>
2020-11-02 19:43:20 +0200

    adding integration test

---
+++ src/main/java/com/breweryclient/breweryclient/web/model/CustomerDto.java
@@ -0,0 +1,11 @@
+package com.breweryclient.breweryclient.web.model;
+
+import java.util.UUID;
+
+public class CustomerDto {
+
+        private UUID id;
+        private String name;
+    }
+
+
---
+++ src/test/java/com/breweryclient/breweryclient/BreweryClientApplicationTest.java
@@ -0,0 +1,25 @@
+package com.breweryclient.breweryclient;
+
+import com.breweryclient.breweryclient.web.client.BreweryClient;
+import com.breweryclient.breweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+}
