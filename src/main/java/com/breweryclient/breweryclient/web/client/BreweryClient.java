91c4fd748c83de6c7aad6f8cfc910e16b677e447
DonneGri <grigalionyte.donata@gmail.com>
2020-11-22 21:57:24 +0200

    add post,put & delete

--- src/main/java/com/breweryclient/breweryclient/web/client/BreweryClient.java
+++ src/main/java/com/breweryclient/breweryclient/web/client/BreweryClient.java
@@ -5,8 +5,10 @@ import com.breweryclient.breweryclient.web.model.BeerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -23,6 +25,22 @@ public class BreweryClient {
 
     public BeerDto getBeerById(UUID uuid){
         return  restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(),BeerDto.class);
+    }
+
+    public URI saveNewBeer(@RequestBody BeerDto beerDto){
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
+    public void updateBeer(UUID uuid, BeerDto beerDto){
+
+        restTemplate.put(apihost+BEER_PATH_V1+"/"+uuid.toString(),beerDto);
+
+    }
+
+    public void deleteBeer(UUID uuid){
+
+        restTemplate.delete(apihost+BEER_PATH_V1+"/"+ uuid.toString());
+
     }
 
     public void setApihost(String apihost){
--- src/test/java/com/breweryclient/breweryclient/BreweryClientApplicationTest.java
+++ src/test/java/com/breweryclient/breweryclient/BreweryClientApplicationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -17,9 +18,32 @@ class BreweryClientTest {
     BreweryClient client;
 
     @Test
-    void getBeerById() {
+    void testGetBeerById() {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
         assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBeer(){
+
+        BeerDto newBeer =  BeerDto.builder().beerName("Rose Beer").build();
+
+        URI uri = client.saveNewBeer(newBeer);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateBeer(){
+
+        BeerDto beerDto =  BeerDto.builder().beerName("Rose Beer").build();
+        client.updateBeer(UUID.randomUUID(),beerDto);
+
+    }
+
+    @Test
+    void testDeleteBeer(){
+        client.deleteBeer(UUID.randomUUID());
     }
 
 }
